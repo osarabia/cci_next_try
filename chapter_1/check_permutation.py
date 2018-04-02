242e4b3640f911f41190087457364310cdd87b07
Omar Sarabia <osarabia@contractor.ea.com>
2018-04-02 12:21:19 -0700

    w/o counter dependency

--- chapter_1/check_permutation.py
+++ chapter_1/check_permutation.py
@@ -1,13 +1,24 @@
-from collections import Counter
-
 
 def check_permutation(string1, string2):
 
     if len(string1) != len(string2):
         return False
 
-    counter_s1 = Counter(string1)
-    counter_s2 = Counter(string2)
+    counter_s1, counter_s2 = {}, {}
+
+    for c1 in string1:
+        if c1 in counter_s1:
+            counter_s1[c1] += 1
+        else:
+            counter_s1[c1] = 1
+
+    print(counter_s1)
+    for c2 in string2:
+        if c2 in counter_s2:
+            counter_s2[c2] += 1
+        else:
+            counter_s2[c2] = 1
+    print(counter_s2)
 
     for c in counter_s1:
         if counter_s1[c] != counter_s2[c]:
