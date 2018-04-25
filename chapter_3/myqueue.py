f401f1363e4c1f0f6b0886586be0e48e1babc6b1
Omar Sarabia <elsarabia@hotmail.com>
2018-04-24 19:42:18 -0700

    myqueue test case

--- chapter_3/myqueue.py
+++ chapter_3/myqueue.py
@@ -1,11 +1,12 @@
+import unittest
 
 class MyQueue(object):
     def __init__(self):
         self.stack_in = []
         self.stack_out = []
 
-    def enqueue(self, item);
-        self.stack_in.push(item)
+    def enqueue(self, item):
+        self.stack_in.append(item)
 
     def dequeue(self):
         if len(self.stack_out) == 0:
@@ -14,6 +15,22 @@ class MyQueue(object):
 
             while len(self.stack_in) > 0:
                 val = self.stack_in.pop()
-                self.stack_out.push(val)
+                self.stack_out.append(val)
 
         return self.stack_out.pop()
+
+class Tests(unittest.TestCase):
+    def test_stacks(self):
+        queue = MyQueue()
+        in_sto = []
+        for i in range(35):
+            queue.enqueue(i)
+            in_sto.append(i)
+        lst = []
+        for _ in range(35):
+            lst.append(queue.dequeue())
+        self.assertEqual(in_sto, lst)
+
+
+if __name__ == '__main__':
+    unittest.main()
