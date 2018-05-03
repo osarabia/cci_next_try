0bdc1066fe231164ab82b5e1cb0595063c005b21
Omar Sarabia <elsarabia@hotmail.com>
2018-05-02 22:29:09 -0700

    change double assignation

--- chapter_4/src/linkedlist/linkedlist.go
+++ chapter_4/src/linkedlist/linkedlist.go
@@ -1,5 +1,7 @@
 package linkedlist
 
+import "fmt"
+
 type Node struct{
     prev *Node
     next *Node
@@ -11,15 +13,25 @@ type List struct{
     tail *Node
 }
 
+func (N *Node) String() string{
+    return fmt.Sprintf("%d", N.value)
+}
+func (N *Node) Next() *Node{
+    return N.next
+}
+
 func (L *List) Insert(value interface{}){
-    node := &Node{
-        value: value
-    }
+    node := &Node{value: value}
 
     if L.head != nil{
         L.tail.next = node
         L.tail = L.tail.next
     } else {
-        L.head = L.tail = node
+        L.head = node
+        L.tail = node
     }
+}
+
+func (L *List) GetHead() *Node{
+    return L.head
 }
