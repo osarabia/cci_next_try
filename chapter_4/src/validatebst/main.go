17b7c9b247c628adeb34278d666bc22a6aeb7c2f
Omar <elomra@gmail.com>
2018-05-06 23:09:09 -0700

    validate binary tree is a binary search tree

--- chapter_4/src/validatebst/main.go
+++ chapter_4/src/validatebst/main.go
@@ -9,7 +9,65 @@ func main(){
     fmt.Println("Starting")
     bt := createBT()
     leftSub := isLeftBalanced(bt)
+    rightSub := isRightBalanced(bt)
     fmt.Println(leftSub)
+    fmt.Println(rightSub)
+}
+
+func isRightBalanced(rootNode *node.Node) bool{
+    var max, min, leftVal, rightVal,Val int
+    min = rootNode.GetInt()
+
+    Val = rootNode.GetInt()
+    if rootNode.Right != nil {
+       rightVal = rootNode.Right.GetInt()
+       if rightVal > Val {
+           max = rightVal
+       } else {
+           return false
+       }
+    }
+
+    array := []*node.Node{rootNode.Right}
+
+    for len(array) > 0 {
+        array_children := make([]*node.Node, 0, 5)
+
+        for idx, rN := range array{
+             Val = rN.GetInt()
+             // evaluating left
+             if rN.Left != nil {
+                 leftVal = rN.Left.GetInt()
+                 if leftVal > Val || leftVal < min {
+                     return false
+                 }
+                 array_children = append(array_children, rN.Left)
+             }
+
+             // evaluating Right
+             if rN.Right != nil{
+                 rightVal = rN.Right.GetInt()
+                 if rightVal <= Val{
+                     return false
+                 }
+                 if idx < len(array) - 1{
+                     array_children = append(array_children, rN.Right)
+                     continue
+                 }
+                 if idx == len(array) - 1 && rightVal > max{
+                     max = rightVal
+                     array_children = append(array_children, rN.Right)
+                 } else {
+                     return false
+                 }
+             }
+        }
+
+        array = array_children
+    }
+
+    return true
+
 }
 
 func isLeftBalanced(rootNode *node.Node) bool{
@@ -36,15 +94,19 @@ func isLeftBalanced(rootNode *node.Node) bool{
              // evaluating left
              if rN.Left != nil {
                  leftVal = rN.Left.GetInt()
-                 if idx == 0 && leftVal <= Val && leftVal <  min {
-                     min = leftVal
-                 } else if idx > 0 && leftVal <= Val {
-                     fmt.Println("Si")
-                 }else{
-                     fmt.Println("left:", leftVal)
+                 if leftVal > Val {
                      return false
                  }
-                 array_children = append(array_children, rN.Left)
+                 if idx > 0 {
+                     array_children = append(array_children, rN.Left)
+                     continue
+                 }
+                 if idx == 0 && leftVal <  min {
+                     min = leftVal
+                     array_children = append(array_children, rN.Left)
+                 } else {
+                     return false
+                 }
              }
 
              // evaluating Right
@@ -67,13 +129,13 @@ func isLeftBalanced(rootNode *node.Node) bool{
 func createBT() *node.Node{
     root := &node.Node{Value: 5}
     root.Left = &node.Node{Value:3}
-    root.Right = &node.Node{Value:6}
+    root.Right = &node.Node{Value:7}
     left := root.Left
     left.Left = &node.Node{Value:1}
     left.Right = &node.Node{Value:4}
 
     right := root.Right
-    right.Left = &node.Node{Value:20}
+    right.Left = &node.Node{Value:6}
     right.Right = &node.Node{Value:21}
 
     return root
