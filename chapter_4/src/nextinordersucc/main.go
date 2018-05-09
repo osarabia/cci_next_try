3090548bb56e0d3de96dc9dfb901cbfa9938575f
Omar <elomra@gmail.com>
2018-05-08 22:06:41 -0700

    next in order successor

--- chapter_4/src/nextinordersucc/main.go
+++ chapter_4/src/nextinordersucc/main.go
@@ -9,33 +9,73 @@ func main(){
     fmt.Println("Starting")
 
     m := insert(nil, 20)
-    m = insert(m.Node, 8)
-    m = insert(m.Node, 22)
-    m = insert(m.Node, 4)
-    m = insert(m.Node, 12)
-    m = insert(m.Node, 10)
-    m = insert(m.Node, 10)
-    fmt.Println(m.Value)
-    fmt.Println(m.Left.Value)
-    fmt.Println(m.Right.Value)
+    m = insert(m, 8)
+    m = insert(m, 22)
+    m = insert(m, 4)
+    m = insert(m, 12)
+    m = insert(m, 10)
+    m = insert(m, 14)
+
+    successor := nextInOrder(m.Left)
+    fmt.Println(successor.GetInt()) //should Print 10
+
+    successor = nextInOrder(m.Left.Right.Left)
+    fmt.Println(successor.GetInt())//should Print 12
+
+    successor = nextInOrder(m.Left.Right.Right)
+    fmt.Println(successor.GetInt())//should Print 20
 }
 
-func insert(n *node.Node, value int) *node.NodeInOrderSucc {
-    if n == nil{
-        tmp := &node.Node{Value: value}
+func nextInOrder(n *node.Node) *node.Node {
+    //If right subtree of node is not NULL, then succ lies in right subtree
+    if n.Right != nil {
+        return leftMost(n.Right)
+    }
 
-        return &node.NodeInOrderSucc{Node: tmp}
+    //Travel up using the parent pointer
+    p := n.Parent
+    for p != nil {
+
+        //until you see a node which is left child of it’s parent. The parent of such a node is the succ.
+        if n.GetInt() != p.Right.GetInt(){
+            break
+        }
+        n = p
+        p = p.Parent
+    }
+
+    return p
+}
+
+//looking for the left most :)
+func leftMost(n *node.Node) *node.Node {
+    current := n
+
+    for current != nil {
+        if current.Left == nil {
+            break
+        }
+
+        current = current.Left
+    }
+
+    return current
+}
+
+func insert(n *node.Node, value int) *node.Node {
+    if n == nil{
+        return &node.Node{Value: value}
     }
 
     if value <= n.GetInt() {
         tmp := insert(n.Left, value)
-        n.Left = tmp.Node
+        n.Left = tmp
         tmp.Parent = n
     } else {
         tmp := insert(n.Right, value)
-        n.Right = tmp.Node
+        n.Right = tmp
         tmp.Parent = n
     }
 
-    return &node.NodeInOrderSucc{Node: n}
+    return n
 }
