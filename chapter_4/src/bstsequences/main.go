f94b7260348ee4acd60d8376954f17a843500f83
Omar <elomra@gmail.com>
2018-05-15 19:16:24 -0700

    divide and conquer approach

--- chapter_4/src/bstsequences/main.go
+++ chapter_4/src/bstsequences/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func main(){
     fmt.Println("Starting")
+
+    root := createBST()
+    results := findSequences(root)
+}
+
+func createBST() *node.Node{
+    root := &node.Node{Value: 5}
+    root.Left = &node.Node{Value:3}
+    root.Right = &node.Node{Value:7}
+    left := root.Left
+    left.Left = &node.Node{Value:1}
+    left.Right = &node.Node{Value:4}
+
+    right := root.Right
+    right.Left = &node.Node{Value:6}
+    right.Right = &node.Node{Value:21}
+
+    return root
+}
+
+func findSequences(n *node.Node) []int{
+    if n == nil {
+        var empty []int
+
+        return  empty
+    }
+
+    //Leaf Nodes
+    if n.Left && n.Right {
+        return []int{n.GetInt()}
+    }
+
+    left := findSequences(n.Left)
+    right := findSequences(n.Right)
+
+    //merging everything
+    return []int{0}
 }
